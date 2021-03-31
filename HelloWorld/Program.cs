480dc67b8eb7e3ef28f41b60dcdf5eab356fe23c
ryan jawad <rbjawad@ryans-MBP.attlocal.net>
2021-03-31 10:13:01 -0600

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using sc = System.Console;
+
+namespace Revature.Week1.HelloWorld
+{
+  //class, struct, interface, enum only sopes available to the namespace
+
+  class Program // class scope
+  {
+    private static void Main() //method scope
+    {
+      // stdin to get your name
+      sc.WriteLine("enter your name:");
+      // string in;
+      var input = sc.ReadLine(); // datatype inference
+      // object fred2 = System.Console.ReadLine(); //anonymous datatype
+      //stdout to print your name
+      sc.WriteLine($"Hello my name is {input}"); // string interpolation, template string
+    }
+    private void UiPath()
+    {
+      { // block scope
+      }
+
+      { // block scope
+      }
+    }
+  }
+}
